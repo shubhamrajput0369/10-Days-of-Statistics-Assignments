193909c7c1af99d49b0cd79b7a6af3d97a6d0d88
Shubham Rajput <shubham.rajput0369@gmail.com>
2021-04-16 21:15:28 +0530

    Update 0.2 - Day 0: Weighted Mean.cpp

--- 0.2 - Day 0: Weighted Mean.cpp
+++ 0.2 - Day 0: Weighted Mean.cpp
@@ -8,3 +8,12 @@ string ltrim(const string &);
 string rtrim(const string &);
 vector<string> split(const string &);
 
+
+/*
+ * Complete the 'weightedMean' function below.
+ *
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY X
+ *  2. INTEGER_ARRAY W
+ */
+
